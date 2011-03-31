b783788f91525d12b2168109131a627aa24d2ffe
Maciej Szczytowski <maciej.szczytowski@qcadoo.com>
2011-03-31 12:09:55 +0200

    RF# copy resources from plugins

---
+++ qcadoo-view/src/main/java/com/qcadoo/view/internal/resource/JspResourceResolver.java
@@ -0,0 +1,83 @@
+package com.qcadoo.view.internal.resource;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JspResourceResolver implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JspResourceResolver.class);
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Value("${QCADOO_WEBAPP_PATH}")
+    private String webappPath;
+
+    @Value("${useJarStaticResources}")
+    private boolean useJarStaticResources;
+
+    @Override
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+        if (!useJarStaticResources) {
+            return;
+        }
+
+        LOG.info("Copying jsps ...");
+
+        try {
+            Resource[] resources = applicationContext.getResources("classpath*:WEB-INF/jsp/**/*");
+
+            for (Resource resource : resources) {
+                copyResource(resource);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot find resources jsp in classpath", e);
+        }
+    }
+
+    private void copyResource(final Resource resource) {
+        if (!resource.isReadable()) {
+            return;
+        }
+
+        try {
+            String path = resource.getURI().toString().split("WEB-INF/jsp/")[1];
+            File file = new File(webappPath + "/WEB-INF/jsp/" + path);
+
+            if (resource.getInputStream().available() == 0) {
+                file.mkdirs();
+            } else {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Copying " + path + " to " + file.getAbsolutePath());
+                }
+
+                OutputStream output = null;
+
+                try {
+                    output = new BufferedOutputStream(new FileOutputStream(file));
+                    IOUtils.copy(resource.getInputStream(), output);
+                } finally {
+                    IOUtils.closeQuietly(output);
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot copy resource " + resource, e);
+        }
+    }
+
+}
