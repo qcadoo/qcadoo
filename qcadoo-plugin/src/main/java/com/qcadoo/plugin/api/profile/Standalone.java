48fc72e3f4323c3bb8cab158341de57fa66250f1
Mateusz Dymczyk <mateusz.dymczyk@qcadoo.com>
2011-04-06 13:16:26 +0200

    changes in qcadoo-maven-plugin

--- qcadoo-plugin/src/main/java/com/qcadoo/plugin/api/profile/Standalone.java
+++ qcadoo-plugin/src/main/java/com/qcadoo/plugin/api/profile/Standalone.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.context.annotation.Profile;
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Profile("standalone")
