ab92a633ea264b502052181f0e7ea32b1dba4746
Michal Nawalany <m.nawalant@gmail.com>
2011-07-20 08:24:40 +0200

    RF#QCADOOMES-152

--- qcadoo-view/src/main/resources/qcadooView/public/js/core/lib/jquery.form.js
+++ qcadoo-view/src/main/resources/qcadooView/public/js/core/lib/jquery.form.js
@@ -414,9 +414,9 @@ $.fn.ajaxSubmit = function(options) {
 				}
 				
 				// QCADOO remove <pre> tags - begin
-				var hasPre = /^<pre[^>]*>.*<\/pre>$/.test(xhr.responseText);
+				var hasPre = /^<(pre|PRE)[^>]*>.*<\/(pre|PRE)>$/.test(xhr.responseText);
 				if (hasPre) {
-					xhr.responseText = xhr.responseText.replace(/^<pre[^>]*>/,"").replace(/<\/pre>$/,"");
+					xhr.responseText = xhr.responseText.replace(/^<(pre|PRE)[^>]*>/,"").replace(/<\/(pre|PRE)>$/,"");
 				}
 				// QCADOO remove <pre> tags - end
 				
