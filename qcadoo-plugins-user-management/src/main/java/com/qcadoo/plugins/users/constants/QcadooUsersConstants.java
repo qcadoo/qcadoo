dbf7cffe01ace33c890a976bdc115b86fdf617c3
Lukasz Poniewierski <lukasz.poniewierski@gmail.com>
2011-11-24 17:30:09 +0100

    RF Fixed some checkstyle problems.

--- qcadoo-plugins-user-management/src/main/java/com/qcadoo/plugins/users/constants/QcadooUsersConstants.java
+++ qcadoo-plugins-user-management/src/main/java/com/qcadoo/plugins/users/constants/QcadooUsersConstants.java
@@ -25,16 +25,16 @@ package com.qcadoo.plugins.users.constants;
 
 public interface QcadooUsersConstants {
 
-    public static final String PLUGIN_IDENTIFIER = "qcadooUsers";
+    String PLUGIN_IDENTIFIER = "qcadooUsers";
 
     // VIEW
-    public static final String VIEW_PROFILE = "profile";
+    String VIEW_PROFILE = "profile";
 
-    public static final String VIEW_PROFILE_CHANGE_PASSWORD = "profileChangePassword";
+    String VIEW_PROFILE_CHANGE_PASSWORD = "profileChangePassword";
 
-    public static final String VIEW_USER_CHANGE_PASSWORD = "userChangePassword";
+    String VIEW_USER_CHANGE_PASSWORD = "userChangePassword";
 
-    public static final String VIEW_USER_DETAILS = "userDetails";
+    String VIEW_USER_DETAILS = "userDetails";
 
-    public static final String VIEW_USERS_LIST = "usersList";
+    String VIEW_USERS_LIST = "usersList";
 }
