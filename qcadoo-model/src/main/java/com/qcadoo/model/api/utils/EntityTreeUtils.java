b22b3d9395f004406ded31102b3070feed5f813b
Marcin Kubala <marcin.kubala@qcadoo.com>
2011-12-05 16:19:15 +0100

    SC# add DetachedEntityTree, change EntityTreeUtils

--- qcadoo-model/src/main/java/com/qcadoo/model/api/utils/EntityTreeUtils.java
+++ qcadoo-model/src/main/java/com/qcadoo/model/api/utils/EntityTreeUtils.java
@@ -1,34 +1,18 @@
 package com.qcadoo.model.api.utils;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import java.util.List;
 
 import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.EntityTreeNode;
-import com.qcadoo.model.internal.EntityTreeNodeImpl;
+import com.qcadoo.model.api.EntityTree;
+import com.qcadoo.model.internal.DetachedEntityTreeImpl;
 
 public final class EntityTreeUtils {
 
     private EntityTreeUtils() {
     }
 
-    public static EntityTreeNode getEntityTreeNode(final Entity entity, final EntityTreeNode parentNode) {
-        int priority = 1;
-        if (parentNode != null) {
-            priority = parentNode.getChildren().size() + 1;
-        }
-        return getEntityTreeNode(entity, parentNode, priority);
-    }
-
-    public static EntityTreeNode getEntityTreeNode(final Entity entity, final EntityTreeNode parentNode, final int priority) {
-        checkArgument(entity != null, "given entity should not be null!");
-
-        EntityTreeNode node = new EntityTreeNodeImpl(entity);
-        node.setField("priority", priority);
-        if (parentNode != null) {
-            node.setField("parent", parentNode);
-            ((EntityTreeNodeImpl) parentNode).addChild(node);
-        }
-        return node;
+    public static EntityTree getDetachedEntityTree(final List<Entity> entities) {
+        return new DetachedEntityTreeImpl(entities);
     }
 
 }
