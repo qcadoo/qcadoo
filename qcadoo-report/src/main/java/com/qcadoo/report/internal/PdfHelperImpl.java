b3b2b9e2b2cc23ad6452a4a37eadddcb66c77c24
Alina Brauer <alina.brauer@qcadoo.com>
2013-01-08 14:38:03 +0100

    SC#QCADOOMESCLS-510 add missing autowired

--- qcadoo-report/src/main/java/com/qcadoo/report/internal/PdfHelperImpl.java
+++ qcadoo-report/src/main/java/com/qcadoo/report/internal/PdfHelperImpl.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.lowagie.text.BadElementException;
@@ -61,6 +62,7 @@ public final class PdfHelperImpl implements PdfHelper {
 
     private static final Logger LOG = LoggerFactory.getLogger(PdfHelperImpl.class);
 
+    @Autowired
     private TranslationService translationService;
 
     @Override
