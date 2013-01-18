e078fae1e75777b848c63324ae0e04b958a60cc7
Marcin Kubala <marcin.kubala@qcadoo.com>
2013-01-18 10:55:34 +0100

    CM qcadooPlugin: update JavaDoc for @RunIfEnabled

--- qcadoo-plugin/src/main/java/com/qcadoo/plugin/api/RunIfEnabled.java
+++ qcadoo-plugin/src/main/java/com/qcadoo/plugin/api/RunIfEnabled.java
@@ -30,21 +30,28 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+
 /**
- * Determines execution of annotated (type's) method. Run method(s) only if all the plugins with given identifiers are enabled.
+ * Determines execution of annotated (type's) method. Run method(s) only if all specified plug-ins are enabled.
  * 
- * This annotation also works with aspects and advices.
+ * Annotating whole class/aspect is equivalent to annotating each method/advice (also setters and getters!)
+ * 
+ * This annotation also works with aspects. When applied to {@link Around} advice and required plug-in(s) is not enabled then
+ * proceed given {@link ProceedingJoinPoint} omitting thereby execution of advice's body.
  * 
  * @since 1.1.7
+ * @author Marcin Kubala
  */
 @Retention(RUNTIME)
 @Target(value = { TYPE, METHOD })
 public @interface RunIfEnabled {
 
     /**
-     * Plugin identifiers
+     * Plug-in identifiers
      * 
-     * @return plugin identifiers
+     * @return plug-in identifiers
      */
     String[] value();
 
