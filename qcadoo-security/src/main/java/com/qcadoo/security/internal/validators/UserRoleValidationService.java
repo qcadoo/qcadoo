e8f5aba76caf7daa9701af38acc478f589dada26
Marcin Kubala <marcin.kubala@qcadoo.com>
2013-01-22 17:51:17 +0100

    DF#QCADOO-304 qcadooSecurity: fix problem with user entity validation (role field)

--- qcadoo-security/src/main/java/com/qcadoo/security/internal/validators/UserRoleValidationService.java
+++ qcadoo-security/src/main/java/com/qcadoo/security/internal/validators/UserRoleValidationService.java
@@ -1,12 +1,16 @@
 package com.qcadoo.security.internal.validators;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Objects;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.FieldDefinition;
 import com.qcadoo.security.api.SecurityService;
 import com.qcadoo.security.constants.QcadooSecurityConstants;
+import com.qcadoo.security.constants.UserFields;
 
 @Service
 public class UserRoleValidationService {
@@ -15,11 +19,41 @@ public class UserRoleValidationService {
     private SecurityService securityService;
 
     public boolean checkUserCreatingSuperadmin(final DataDefinition dataDefinition, final Entity entity) {
-        String role = entity.getStringField("role");
-        if (QcadooSecurityConstants.ROLE_SUPERADMIN.equals(role) && securityService.getCurrentUserName() != null) {
-            entity.addError(dataDefinition.getField("role"), "qcadooUsers.validate.global.error.forbiddenRole");
-            return false;
-        }
-        return true;
+        final Object newValue = entity.getField(UserFields.ROLE);
+        final Object oldValue = getOldValue(dataDefinition, entity);
+        return checkUserCreatingSuperadmin(dataDefinition, dataDefinition.getField(UserFields.ROLE), entity, oldValue, newValue);
     }
+
+    private Object getOldValue(final DataDefinition dataDefinition, final Entity entity) {
+        if (entity.getId() == null) {
+            return null;
+        } else {
+            final Entity existingEntity = dataDefinition.get(entity.getId());
+            return existingEntity.getField(UserFields.ROLE);
+        }
+    }
+
+    private boolean checkUserCreatingSuperadmin(final DataDefinition dataDefinition, final FieldDefinition fieldDefinition,
+            final Entity entity, final Object oldValue, final Object newValue) {
+        if (Objects.equal(oldValue, newValue) || isCurrentUserShopOrSuperAdmin(entity, dataDefinition)
+                || !QcadooSecurityConstants.ROLE_SUPERADMIN.equals(newValue)) {
+            return true;
+        }
+        entity.addError(fieldDefinition, "qcadooUsers.validate.global.error.forbiddenRole");
+        return false;
+    }
+
+    private boolean isCurrentUserShopOrSuperAdmin(final Entity savingUserEntity, final DataDefinition userDataDefinition) {
+        if (isCalledFromShop()) {
+            return true;
+        }
+        final Long currentUserId = securityService.getCurrentUserId();
+        final Entity currentUserEntity = userDataDefinition.get(currentUserId);
+        return QcadooSecurityConstants.ROLE_SUPERADMIN.equals(currentUserEntity.getStringField(UserFields.ROLE));
+    }
+
+    private boolean isCalledFromShop() {
+        return SecurityContextHolder.getContext().getAuthentication() == null;
+    }
+
 }
