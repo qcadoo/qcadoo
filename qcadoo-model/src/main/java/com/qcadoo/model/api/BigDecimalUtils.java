9e2d0062172accbc38032bb1b68c2e9b715360ef
Kamil Siwonia <kamil.siwonia@qcadoo.com>
2013-03-01 12:57:41 +0100

    SC#QCADOO-343 Add BigDecimalUtils

---
+++ qcadoo-model/src/main/java/com/qcadoo/model/api/BigDecimalUtils.java
@@ -0,0 +1,63 @@
+/**
+ * ***************************************************************************
+ * Copyright (c) 2010 Qcadoo Limited
+ * Project: Qcadoo Framework
+ * Version: 1.2.0
+ *
+ * This file is part of Qcadoo.
+ *
+ * Qcadoo is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * ***************************************************************************
+ */
+package com.qcadoo.model.api;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public final class BigDecimalUtils {
+
+    private static BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100L);
+
+    private BigDecimalUtils() {
+
+    }
+
+    public static BigDecimal convertNullToZero(final Object value) {
+        if (value == null) {
+            return BigDecimal.ZERO;
+        }
+        if (value instanceof BigDecimal) {
+            return (BigDecimal) value;
+        }
+        return BigDecimal.valueOf(Double.valueOf(value.toString()));
+    }
+
+    public static BigDecimal convertNullToOne(final Object value) {
+        if (value == null) {
+            return BigDecimal.ONE;
+        }
+        if (value instanceof BigDecimal) {
+            return (BigDecimal) value;
+        }
+        return BigDecimal.valueOf(Double.valueOf(value.toString()));
+
+    }
+
+    public static BigDecimal toPercent(final BigDecimal decimalValue, final MathContext mathCntext) {
+        return convertNullToZero(decimalValue).divide(ONE_HUNDRED, mathCntext);
+
+    }
+
+}
