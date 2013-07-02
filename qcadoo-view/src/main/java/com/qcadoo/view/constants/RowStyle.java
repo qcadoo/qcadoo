1035ee39327ad03619c99367b118d966f276fdb1
Lukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2013-07-02 14:54:43 +0200

    SC#QCADOOMES-1693 Added constants for new css styles.

--- qcadoo-view/src/main/java/com/qcadoo/view/constants/RowStyle.java
+++ qcadoo-view/src/main/java/com/qcadoo/view/constants/RowStyle.java
@@ -29,6 +29,14 @@ public final class RowStyle {
     }
 
     public static final String RED_BACKGROUND = "redBg";
+    
+    public static final String YELLOW_BACKGROUND = "yellowBg";
+    
+    public static final String BROWN_BACKGROUND = "brownBg";
+    
+    public static final String BLUE_BACKGROUND = "blueBg";
+    
+    public static final String GREEN_BACKGROUND = "greenBg";
 
     public static final String BOLD_FONT = "boldFont";
 
