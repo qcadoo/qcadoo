481d9730b0c2aa46de2312fb97356fd06fe172fa
Marcin Kubala <marcin.kubala@qcadoo.com>
2014-03-11 13:50:03 +0100

    SC introduce UserService, aggregate/helper for common user-related operations

---
+++ qcadoo-security/src/main/java/com/qcadoo/security/api/UserService.java
@@ -0,0 +1,37 @@
+package com.qcadoo.security.api;
+
+import com.qcadoo.model.api.Entity;
+
+/**
+ * This service provides common operations for user
+ * 
+ * @since 1.2.1
+ */
+public interface UserService {
+
+    /**
+     * Find user by id
+     * 
+     * @param userId
+     *            id of the user to find
+     * @return matching user entity or null
+     */
+    Entity findUserEntity(final Long userId);
+
+    /**
+     * Get current user entity
+     * 
+     * @return current user entity
+     */
+    Entity getCurrentUserEntity();
+
+    /**
+     * Extract user's first and last names.
+     * 
+     * @param user
+     *            user entity from which full name will be extracted.
+     * @return string containing user's first and last names or null if given user is null.
+     */
+    String extractFullName(final Entity user);
+
+}
