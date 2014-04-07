a9efaea29129dd854991b0d6dbe4e65eb85e4551
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2014-04-07 11:13:39 +0200

    RF# typo fix

--- qcadoo-view/src/main/java/com/qcadoo/view/internal/ribbon/RibbonParserService.java
+++ qcadoo-view/src/main/java/com/qcadoo/view/internal/ribbon/RibbonParserService.java
@@ -51,167 +51,167 @@ import com.qcadoo.view.internal.xml.ViewDefinitionParserNodeException;
 @Service
 public class RibbonParserService {
 
-    private static final String NAME = "name";
+	private static final String NAME = "name";
 
-    private static final String ALIGNMENT = "alignment";
+	private static final String ALIGNMENT = "alignment";
 
-    @Autowired
-    private RibbonTemplatesService ribbonTemplatesService;
+	@Autowired
+	private RibbonTemplatesService ribbonTemplatesService;
 
-    private final RibbonTemplates ribbonTemplates = new RibbonTemplates();
+	private final RibbonTemplates ribbonTemplates = new RibbonTemplates();
 
-    public InternalRibbon parseRibbon(final Node ribbonNode, final ViewDefinitionParser parser,
-            final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
-        InternalRibbon ribbon = new RibbonImpl();
+	public InternalRibbon parseRibbon(final Node ribbonNode, final ViewDefinitionParser parser,
+	        final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
+		InternalRibbon ribbon = new RibbonImpl();
 
-        NodeList childNodes = ribbonNode.getChildNodes();
+		NodeList childNodes = ribbonNode.getChildNodes();
 
-        for (int i = 0; i < childNodes.getLength(); i++) {
-            Node child = childNodes.item(i);
-            if (Node.ELEMENT_NODE != child.getNodeType()) {
-                continue;
-            }
-            if ("group".equals(child.getNodeName())) {
-                ribbon.addGroupsPack(new SingleRibbonGroupPack(parseRibbonGroup(child, parser, viewDefinition)));
-            } else if ("template".equals(child.getNodeName())) {
-                applyTemplate(child, ribbon, parser, viewDefinition);
-            } else {
-                throw new ViewDefinitionParserNodeException(child, "Wrong node type - 'group' or 'temlplate' expected");
-            }
-        }
-        ribbon.setAlignment(parser.getStringAttribute(ribbonNode, ALIGNMENT));
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node child = childNodes.item(i);
+			if (Node.ELEMENT_NODE != child.getNodeType()) {
+				continue;
+			}
+			if ("group".equals(child.getNodeName())) {
+				ribbon.addGroupsPack(new SingleRibbonGroupPack(parseRibbonGroup(child, parser, viewDefinition)));
+			} else if ("template".equals(child.getNodeName())) {
+				applyTemplate(child, ribbon, parser, viewDefinition);
+			} else {
+				throw new ViewDefinitionParserNodeException(child, "Wrong node type - 'group' or 'template' expected");
+			}
+		}
+		ribbon.setAlignment(parser.getStringAttribute(ribbonNode, ALIGNMENT));
 
-        return ribbon;
-    }
+		return ribbon;
+	}
 
-    private void applyTemplate(final Node templateNode, final InternalRibbon ribbon, final ViewDefinitionParser parser,
-            final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
-        String name = parser.getStringAttribute(templateNode, NAME);
-        parser.checkState(name != null, templateNode, "Name attribute cannot be empty");
-        String plugin = parser.getStringAttribute(templateNode, "plugin");
+	private void applyTemplate(final Node templateNode, final InternalRibbon ribbon, final ViewDefinitionParser parser,
+	        final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
+		String name = parser.getStringAttribute(templateNode, NAME);
+		parser.checkState(name != null, templateNode, "Name attribute cannot be empty");
+		String plugin = parser.getStringAttribute(templateNode, "plugin");
 
-        RibbonTemplateParametersBuilder parametersBuilder = RibbonTemplateParameters.getBuilder(plugin, name);
-        try {
-            parametersBuilder.usingOnlyGroups(parser.getStringAttribute(templateNode, "includeGroups"));
-            parametersBuilder.usingOnlyItems(parser.getStringAttribute(templateNode, "includeItems"));
-            parametersBuilder.withoutGroups(parser.getStringAttribute(templateNode, "excludeGroups"));
-            parametersBuilder.withoutItems(parser.getStringAttribute(templateNode, "excludeItems"));
-        } catch (IllegalStateException e) {
-            throw new ViewDefinitionParserNodeException(templateNode, e);
-        }
+		RibbonTemplateParametersBuilder parametersBuilder = RibbonTemplateParameters.getBuilder(plugin, name);
+		try {
+			parametersBuilder.usingOnlyGroups(parser.getStringAttribute(templateNode, "includeGroups"));
+			parametersBuilder.usingOnlyItems(parser.getStringAttribute(templateNode, "includeItems"));
+			parametersBuilder.withoutGroups(parser.getStringAttribute(templateNode, "excludeGroups"));
+			parametersBuilder.withoutItems(parser.getStringAttribute(templateNode, "excludeItems"));
+		} catch (IllegalStateException e) {
+			throw new ViewDefinitionParserNodeException(templateNode, e);
+		}
 
-        ribbonTemplatesService.applyTemplate(ribbon, parametersBuilder.build(), viewDefinition);
-    }
+		ribbonTemplatesService.applyTemplate(ribbon, parametersBuilder.build(), viewDefinition);
+	}
 
-    public InternalRibbonGroup parseRibbonGroup(final Node groupNode, final ViewDefinitionParser parser,
-            final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
-        String template = parser.getStringAttribute(groupNode, "template");
+	public InternalRibbonGroup parseRibbonGroup(final Node groupNode, final ViewDefinitionParser parser,
+	        final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
+		String template = parser.getStringAttribute(groupNode, "template");
 
-        if (template == null) {
-            String groupName = parser.getStringAttribute(groupNode, NAME);
-            if (groupName == null) {
-                throw new ViewDefinitionParserNodeException(groupNode, "Name attribute cannot be empty");
-            }
-            InternalRibbonGroup ribbonGroup = new RibbonGroupImpl(groupName);
+		if (template == null) {
+			String groupName = parser.getStringAttribute(groupNode, NAME);
+			if (groupName == null) {
+				throw new ViewDefinitionParserNodeException(groupNode, "Name attribute cannot be empty");
+			}
+			InternalRibbonGroup ribbonGroup = new RibbonGroupImpl(groupName);
 
-            NodeList childNodes = groupNode.getChildNodes();
+			NodeList childNodes = groupNode.getChildNodes();
 
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node child = childNodes.item(i);
+			for (int i = 0; i < childNodes.getLength(); i++) {
+				Node child = childNodes.item(i);
 
-                if (child.getNodeType() == Node.ELEMENT_NODE) {
-                    ribbonGroup.addItem(parseRibbonItem(child, parser, viewDefinition));
-                }
-            }
+				if (child.getNodeType() == Node.ELEMENT_NODE) {
+					ribbonGroup.addItem(parseRibbonItem(child, parser, viewDefinition));
+				}
+			}
 
-            return ribbonGroup;
-        } else {
-            try {
-                return ribbonTemplates.getGroupTemplate(template, viewDefinition);
-            } catch (IllegalStateException e) {
-                throw new ViewDefinitionParserNodeException(groupNode, e);
-            }
-        }
-    }
+			return ribbonGroup;
+		} else {
+			try {
+				return ribbonTemplates.getGroupTemplate(template, viewDefinition);
+			} catch (IllegalStateException e) {
+				throw new ViewDefinitionParserNodeException(groupNode, e);
+			}
+		}
+	}
 
-    public InternalRibbonActionItem parseRibbonItem(final Node itemNode, final ViewDefinitionParser parser,
-            final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
-        String stringType = itemNode.getNodeName();
+	public InternalRibbonActionItem parseRibbonItem(final Node itemNode, final ViewDefinitionParser parser,
+	        final ViewDefinition viewDefinition) throws ViewDefinitionParserNodeException {
+		String stringType = itemNode.getNodeName();
 
-        RibbonActionItem.Type type = null;
-        if ("bigButtons".equals(stringType) || "bigButton".equals(stringType)) {
-            type = RibbonActionItem.Type.BIG_BUTTON;
-        } else if ("smallButtons".equals(stringType) || "smallButton".equals(stringType)) {
-            type = RibbonActionItem.Type.SMALL_BUTTON;
-        } else if ("combobox".equals(stringType)) {
-            type = RibbonActionItem.Type.COMBOBOX;
-        } else if ("smallEmptySpace".equals(stringType)) {
-            type = RibbonActionItem.Type.SMALL_EMPTY_SPACE;
-        } else {
-            throw new ViewDefinitionParserNodeException(itemNode, "Unsupported ribbon item type '" + stringType + "'");
-        }
+		RibbonActionItem.Type type = null;
+		if ("bigButtons".equals(stringType) || "bigButton".equals(stringType)) {
+			type = RibbonActionItem.Type.BIG_BUTTON;
+		} else if ("smallButtons".equals(stringType) || "smallButton".equals(stringType)) {
+			type = RibbonActionItem.Type.SMALL_BUTTON;
+		} else if ("combobox".equals(stringType)) {
+			type = RibbonActionItem.Type.COMBOBOX;
+		} else if ("smallEmptySpace".equals(stringType)) {
+			type = RibbonActionItem.Type.SMALL_EMPTY_SPACE;
+		} else {
+			throw new ViewDefinitionParserNodeException(itemNode, "Unsupported ribbon item type '" + stringType + "'");
+		}
 
-        InternalRibbonActionItem item = null;
-        if ("bigButtons".equals(stringType) || "smallButtons".equals(stringType)) {
-            item = new RibbonComboItemImpl();
-        } else if ("combobox".equals(stringType)) {
-            item = new RibbonComboBoxImpl();
-        } else {
-            item = new RibbonActionItemImpl();
-        }
+		InternalRibbonActionItem item = null;
+		if ("bigButtons".equals(stringType) || "smallButtons".equals(stringType)) {
+			item = new RibbonComboItemImpl();
+		} else if ("combobox".equals(stringType)) {
+			item = new RibbonComboBoxImpl();
+		} else {
+			item = new RibbonActionItemImpl();
+		}
 
-        item.setIcon(parser.getStringAttribute(itemNode, "icon"));
-        item.setName(parser.getStringAttribute(itemNode, NAME));
-        item.setAction(RibbonUtils.translateRibbonAction(parser.getStringAttribute(itemNode, "action"), viewDefinition));
-        item.setType(type);
-        String state = parser.getStringAttribute(itemNode, "state");
-        if (state == null) {
-            item.setEnabled(true);
-        } else {
-            if ("enabled".equals(state)) {
-                item.setEnabled(true);
-            } else if ("disabled".equals(state)) {
-                item.setEnabled(false);
-            } else {
-                throw new ViewDefinitionParserNodeException(itemNode, "Unsupported ribbon item state : " + state);
-            }
-        }
-        item.setDefaultEnabled(item.isEnabled());
-        String message = parser.getStringAttribute(itemNode, "message");
-        if (message != null) {
-            item.setMessage(message);
-        }
+		item.setIcon(parser.getStringAttribute(itemNode, "icon"));
+		item.setName(parser.getStringAttribute(itemNode, NAME));
+		item.setAction(RibbonUtils.translateRibbonAction(parser.getStringAttribute(itemNode, "action"), viewDefinition));
+		item.setType(type);
+		String state = parser.getStringAttribute(itemNode, "state");
+		if (state == null) {
+			item.setEnabled(true);
+		} else {
+			if ("enabled".equals(state)) {
+				item.setEnabled(true);
+			} else if ("disabled".equals(state)) {
+				item.setEnabled(false);
+			} else {
+				throw new ViewDefinitionParserNodeException(itemNode, "Unsupported ribbon item state : " + state);
+			}
+		}
+		item.setDefaultEnabled(item.isEnabled());
+		String message = parser.getStringAttribute(itemNode, "message");
+		if (message != null) {
+			item.setMessage(message);
+		}
 
-        NodeList childNodes = itemNode.getChildNodes();
-        for (int i = 0; i < childNodes.getLength(); i++) {
-            Node child = childNodes.item(i);
-            if (child.getNodeType() == Node.ELEMENT_NODE && "script".equals(child.getNodeName())) {
-                item.setScript(parser.getStringNodeContent(child));
-            }
-        }
+		NodeList childNodes = itemNode.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node child = childNodes.item(i);
+			if (child.getNodeType() == Node.ELEMENT_NODE && "script".equals(child.getNodeName())) {
+				item.setScript(parser.getStringNodeContent(child));
+			}
+		}
 
-        if (item instanceof RibbonComboItem) {
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node child = childNodes.item(i);
-                if (child.getNodeType() == Node.ELEMENT_NODE && !"script".equals(child.getNodeName())) {
-                    ((InternalRibbonComboItem) item).addItem(parseRibbonItem(child, parser, viewDefinition));
-                }
-            }
-        } else if (item instanceof RibbonComboBox) {
-            for (int i = 0; i < childNodes.getLength(); i++) {
-                Node child = childNodes.item(i);
-                if (child.getNodeType() == Node.ELEMENT_NODE && !"script".equals(child.getNodeName())) {
-                    if (!"option".equals(child.getNodeName())) {
-                        throw new ViewDefinitionParserNodeException(child, "ribbon combobox can only have 'option' elements");
-                    }
-                    ((RibbonComboBox) item).addOption(parser.getStringAttribute(child, NAME));
-                }
-            }
-        } else {
-            (item).setAction(RibbonUtils.translateRibbonAction(parser.getStringAttribute(itemNode, "action"), viewDefinition));
-        }
+		if (item instanceof RibbonComboItem) {
+			for (int i = 0; i < childNodes.getLength(); i++) {
+				Node child = childNodes.item(i);
+				if (child.getNodeType() == Node.ELEMENT_NODE && !"script".equals(child.getNodeName())) {
+					((InternalRibbonComboItem) item).addItem(parseRibbonItem(child, parser, viewDefinition));
+				}
+			}
+		} else if (item instanceof RibbonComboBox) {
+			for (int i = 0; i < childNodes.getLength(); i++) {
+				Node child = childNodes.item(i);
+				if (child.getNodeType() == Node.ELEMENT_NODE && !"script".equals(child.getNodeName())) {
+					if (!"option".equals(child.getNodeName())) {
+						throw new ViewDefinitionParserNodeException(child, "ribbon combobox can only have 'option' elements");
+					}
+					((RibbonComboBox) item).addOption(parser.getStringAttribute(child, NAME));
+				}
+			}
+		} else {
+			(item).setAction(RibbonUtils.translateRibbonAction(parser.getStringAttribute(itemNode, "action"), viewDefinition));
+		}
 
-        return item;
-    }
+		return item;
+	}
 
 }
