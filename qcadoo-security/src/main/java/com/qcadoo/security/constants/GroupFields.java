d8ab7c47bb43cc77c2786bfcdd051117bfb07e88
Tomasz Landowski <tomaszlandowski@MacBook-Pro-Marcin-2.local>
2014-04-28 10:51:10 +0200

    RF# qcadoo security models constants update

--- qcadoo-security/src/main/java/com/qcadoo/security/constants/GroupFields.java
+++ qcadoo-security/src/main/java/com/qcadoo/security/constants/GroupFields.java
@@ -1,16 +1,17 @@
 package com.qcadoo.security.constants;
 
-
 public final class GroupFields {
 
-    private GroupFields(){
+    private GroupFields() {
     }
-    
+
     public static final String NAME_FIELD = "name";
-    
+
     public static final String DESCRIPTION_FIELD = "description";
-    
+
     public static final String ROLES_FIELD = "roles";
-    
+
     public static final String USERS_FIELD = "users";
+
+    public static final String IDENTIFIER_FIELD = "identifier";
 }
---
+++ qcadoo-security/src/main/java/com/qcadoo/security/constants/RoleFields.java
@@ -0,0 +1,14 @@
+package com.qcadoo.security.constants;
+
+public final class RoleFields {
+
+    private RoleFields() {
+
+    }
+
+    public static final String IDENTIFIER_FIELD = "identifier";
+
+    public static final String DESCRIPTION_FIELD = "description";
+
+    public static final String GROUPS_FIELD = "groups";
+}
