5dc0af906ada2bcbd6affc5dbcfa7942a6bbca86
Tomasz Landowski <tomaszlandowski@MacBook-Pro-Marcin-2.local>
2014-05-27 14:32:48 +0200

    RF# QCADOO-280 Dicationary item disabling refactored - copy button removed and name input disabled

--- qcadoo-plugins-dictionary-management/src/main/java/com/qcadoo/plugins/dictionaries/internal/hooks/DictionaryItemDetailsHooks.java
+++ qcadoo-plugins-dictionary-management/src/main/java/com/qcadoo/plugins/dictionaries/internal/hooks/DictionaryItemDetailsHooks.java
@@ -23,18 +23,20 @@
  */
 package com.qcadoo.plugins.dictionaries.internal.hooks;
 
-import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.constants.QcadooModelConstants;
-import com.qcadoo.view.api.ViewDefinitionState;
-import com.qcadoo.view.api.components.FormComponent;
-import com.qcadoo.view.api.components.WindowComponent;
-import com.qcadoo.view.api.ribbon.RibbonActionItem;
+import static com.qcadoo.model.constants.DictionaryItemFields.TECHNICAL_CODE;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.qcadoo.model.constants.DictionaryItemFields.TECHNICAL_CODE;
+import com.qcadoo.model.api.DataDefinitionService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.constants.QcadooModelConstants;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.FieldComponent;
+import com.qcadoo.view.api.components.FormComponent;
+import com.qcadoo.view.api.components.WindowComponent;
+import com.qcadoo.view.api.ribbon.RibbonActionItem;
 
 @Service
 public class DictionaryItemDetailsHooks {
@@ -42,7 +44,7 @@ public class DictionaryItemDetailsHooks {
     @Autowired
     private DataDefinitionService dataDefinitionService;
 
-    public void blockedDeleteOptionWhenDictionaryWasAddFromSystem(final ViewDefinitionState view) {
+    public void blockedActivationOptionWhenDictionaryWasAddFromSystem(final ViewDefinitionState view) {
         FormComponent form = (FormComponent) view.getComponentByReference("form");
         if (form.getEntityId() == null) {
             return;
@@ -55,9 +57,22 @@ public class DictionaryItemDetailsHooks {
 
     protected void changedEnabledButton(final ViewDefinitionState view, final boolean enabled) {
         WindowComponent window = (WindowComponent) view.getComponentByReference("window");
-        RibbonActionItem deleteButton = window.getRibbon().getGroupByName("actions").getItemByName("delete");
-        deleteButton.setEnabled(enabled);
-        deleteButton.requestUpdate(true);
+        RibbonActionItem deactivateButton = window.getRibbon().getGroupByName("states").getItemByName("deactivate");
+        deactivateButton.setEnabled(enabled);
+        deactivateButton.requestUpdate(true);
+        RibbonActionItem activateButton = window.getRibbon().getGroupByName("states").getItemByName("activate");
+        activateButton.setEnabled(enabled);
+        activateButton.requestUpdate(true);
+    }
+
+    public void disableNameEdit(final ViewDefinitionState view) {
+        FormComponent form = (FormComponent) view.getComponentByReference("form");
+        if (form.getEntityId() == null) {
+            return;
+        } else {
+            FieldComponent nameFieldComponent = (FieldComponent) view.getComponentByReference("name");
+            nameFieldComponent.setEnabled(false);
+        }
     }
 
     public void disableDictionaryItemFormForExternalItems(final ViewDefinitionState state) {
