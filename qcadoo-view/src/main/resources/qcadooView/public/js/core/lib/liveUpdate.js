c53777f8d787450a8d9fdd57825ef32a9c6ec7be
Katarzyna Małyszek <katarzyna.malyszek@qcadoo.com>
2015-07-15 11:22:20 +0200

    SC# QCADOOCLS-4240 removed Main position from menu, fixed keyboard navigation in search result

--- qcadoo-view/src/main/resources/qcadooView/public/js/core/lib/liveUpdate.js
+++ qcadoo-view/src/main/resources/qcadooView/public/js/core/lib/liveUpdate.js
@@ -1,95 +1,102 @@
-jQuery.fn.liveUpdate = function(list){
-  list = jQuery(list);
+jQuery.fn.liveUpdate = function(list) {
+	list = jQuery(list);
 
-  if ( list.length ) {
-    var rows = list.children('li').clone(),
-      cache = rows.map(function(){
-        return $(this).text();
-      }),
-      $searchResult = $('#searchResult'),
-      $emptySearchResult = $('#emptySearchResult'),
-      $headerSearchForm = $('.headerSearchForm'),
-      $headerMenuContent = $('.headerMenuContent');
+	if (list.length) {
+		var rows = list.children('li').clone(), cache = rows.map(function() {
+			return $(this).text();
+		}), $searchResult = $('#searchResult'), $emptySearchResult = $('#emptySearchResult'), $headerSearchForm = $('.headerSearchForm'), $headerMenuContent = $('.headerMenuContent');
 
-    this
-      .keyup(filter).keyup()
-      .keydown(move).keydown()
-      .parents('form').submit(function(){
-        return false;
-      });
-  }
+		this.keyup(filter).keyup().keydown(move).keydown().parents('form')
+				.submit(function() {
+					return false;
+				});
+	}
 
-  return this;
+	return this;
 
-  function move(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if($.trim($(event.currentTarget).val()).length > 0){
-      if(keycode == '40'){
-        var index = $('a', $searchResult).index($('a.active', $searchResult));
-        if(index == $('a', $searchResult).length - 1){
-          index = -1;
-        }
-        $('a.active', $searchResult).removeClass('active');
-        $('a:eq(' + ( index + 1 ) + ')', $searchResult).addClass('active');
-        return false;
+	function move(event) {
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if ($.trim($(event.currentTarget).val()).length > 0) {
+			if (keycode == '40') {
+				var index = $('a', $searchResult).index(
+						$('a.active', $searchResult));
+				if (index == $('a', $searchResult).length - 1) {
+					index = -1;
+				}
+				$('a.active', $searchResult).removeClass('active');
+				$('a:eq(' + (index + 1) + ')', $searchResult)
+						.addClass('active');
+				return false;
 
-      } else if(keycode == '38'){
-        var index = $('a', $searchResult).index($('a.active', $searchResult));
-        if(index <= 0){
-          index =  $('a', $searchResult).length;
-        }
-        $('a.active', $searchResult).removeClass('active');
-        $('a:eq(' + ( index - 1 ) + ')', $searchResult).addClass('active');
-        return false;
-      }
-      if(keycode == '13'){
-        if($('a.active', $searchResult).length > 0){
-            var href = $('a.active', $searchResult).attr('href');
-            openPage(href);
-        }
-      }
-    }
-  }
+			} else if (keycode == '38') {
+				var index = $('a', $searchResult).index(
+						$('a.active', $searchResult));
+				if (index <= 0) {
+					index = $('a', $searchResult).length;
+				}
+				$('a.active', $searchResult).removeClass('active');
+				$('a:eq(' + (index - 1) + ')', $searchResult)
+						.addClass('active');
+				return false;
+			}
+			if (keycode == '13') {
+				if ($('a.active', $searchResult).length > 0) {
+					//					var href = $('a.active', $searchResult).attr('href');
+					//					openPage(href);
 
-  function filter(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
+					var href = $('a.active', $searchResult).parent().attr('id');
+					var itemParts = href.split("_");
+					$('.userMenuBackdoor').click();
+					windowController.goToMenuPosition(itemParts[1] + "."
+							+ itemParts[2]);
+				}
+			}
+		}
+	}
 
-    if(keycode != '40' && keycode != '38'){
-      var term = jQuery.trim( jQuery(this).val() ), scores = [];
+	function filter(event) {
+		var keycode = (event.keyCode ? event.keyCode : event.which);
 
-      $emptySearchResult.hide();
-      $headerSearchForm.removeClass('showClear');
-      $headerMenuContent.removeClass('openSearchResult');
-      $(".active", $searchResult).removeClass('active');
-      $searchResult.removeHighlight();
-      $("li", $searchResult).remove();
-      if ( term ) {
-        $headerSearchForm.addClass('showClear');
-        $headerMenuContent.addClass('openSearchResult');
+		if (keycode != '40' && keycode != '38') {
+			var term = jQuery.trim(jQuery(this).val()), scores = [];
 
-        cache.each(function(i){
-          var score = this.search(new RegExp(term, "i"));
-          if (score > -1 ) { scores.push([score, i]); }
-        });
+			$emptySearchResult.hide();
+			$headerSearchForm.removeClass('showClear');
+			$headerMenuContent.removeClass('openSearchResult');
+			$(".active", $searchResult).removeClass('active');
+			$searchResult.removeHighlight();
+			$("li", $searchResult).remove();
+			if (term) {
+				$headerSearchForm.addClass('showClear');
+				$headerMenuContent.addClass('openSearchResult');
 
-        jQuery.each(scores, function(){
-          $(".subMenu", $searchResult).append(rows[ this[1] ]);
-        });
+				cache.each(function(i) {
+					var score = this.search(new RegExp(term, "i"));
+					if (score > -1) {
+						scores.push([score, i]);
+					}
+				});
 
-        if(scores.length < 1){
-         $emptySearchResult.show();
-        } else {
-          $("li", $searchResult).click(function(e){
-            var href = $(this).attr('id');
-            var itemParts = href.split("_");
-            windowController.goToMenuPosition(itemParts[1] + "." + itemParts[2]);
-            //openPage(href);
-            e.preventDefault();
-          });
-        }
+				jQuery.each(scores, function() {
+					$(".subMenu", $searchResult).append(rows[this[1]]);
+				});
 
-        $searchResult.highlight(term);
-      }
-    }
-  }
+				if (scores.length < 1) {
+					$emptySearchResult.show();
+				} else {
+					$("li", $searchResult).click(
+							function(e) {
+								var href = $(this).attr('id');
+								var itemParts = href.split("_");
+								windowController.goToMenuPosition(itemParts[1]
+										+ "." + itemParts[2]);
+								//openPage(href);
+								e.preventDefault();
+							});
+				}
+
+				$searchResult.highlight(term);
+			}
+		}
+	}
 };
