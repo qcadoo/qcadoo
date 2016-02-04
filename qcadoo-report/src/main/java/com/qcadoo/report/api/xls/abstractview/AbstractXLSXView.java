d1541c3bad6114c2a088b36cd5da29f6cdcd4406
ksiwonia <kamil.siwonia@gmial.com>
2016-02-04 18:40:32 +0100

    DF#QCADOOCLS-4637

--- qcadoo-report/src/main/java/com/qcadoo/report/api/xls/abstractview/AbstractXLSXView.java
+++ qcadoo-report/src/main/java/com/qcadoo/report/api/xls/abstractview/AbstractXLSXView.java
@@ -1,25 +1,31 @@
 package com.qcadoo.report.api.xls.abstractview;
 
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.servlet.view.AbstractView;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
 import java.util.Map;
 
 public abstract class AbstractXLSXView extends AbstractView {
 
-    /** The content type for an Excel response */
-    private static final String CONTENT_TYPE_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-    private static final String CONTENT_TYPE_XLS = "application/vnd.ms-excel";
 
-    /**
-     * Default Constructor. Sets the content type of the view for excel files.
-     */
+    /** The content type for an Excel response */
+    private static final String CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    /** The extension to look for existing templates */
+    private static final String EXTENSION = ".xlsx";
+    private String url;
+
+    /**	 * Default Constructor.	 * Sets the content type of the view to "application/vnd.ms-excel".	 */
     public AbstractXLSXView() {
+        setContentType(CONTENT_TYPE);
     }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
 
     @Override
     protected boolean generatesDownloadContent() {
@@ -30,53 +36,38 @@ public abstract class AbstractXLSXView extends AbstractView {
      * Renders the Excel view, given the specified model.
      */
     @Override
-    protected final void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
+    protected final void renderMergedOutputModel(
+            Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        Workbook workbook = createWorkbook();
-
-        if (workbook instanceof XSSFWorkbook) {
-            setContentType(CONTENT_TYPE_XLSX);
-        } else {
-            setContentType(CONTENT_TYPE_XLS);
-        }
+        XSSFWorkbook workbook;
+        ByteArrayOutputStream baos = createTemporaryOutputStream();
+		/*if (this.url != null) {
+			workbook = getTemplateSource(this.url, request);
+		}
+		else {*/
+        workbook = new XSSFWorkbook();
+        logger.debug("Created Excel Workbook from scratch");
+        //}
 
         buildExcelDocument(model, workbook, request, response);
 
         // Set the content type.
-        response.setContentType(getContentType());
+        //response.setContentType(getContentType());
+
+        // Should we set the content length here?
+        // response.setContentLength(workbook.getBytes().length);
 
         // Flush byte array to servlet output stream.
-        ServletOutputStream out = response.getOutputStream();
-        out.flush();
-        workbook.write(out);
-        out.flush();
-
-        // Don't close the stream - we didn't open it, so let the container
-        // handle it.
-        // http://stackoverflow.com/questions/1829784/should-i-close-the-servlet-outputstream
+        //ServletOutputStream out = response.getOutputStream();
+        workbook.write(baos);
+        writeToResponse(response, baos);
+        //out.flush();
     }
 
-    /**
-     * Subclasses must implement this method to create an Excel Workbook.
-     * HSSFWorkbook and XSSFWorkbook are both possible formats.
-     */
-    protected abstract Workbook createWorkbook();
 
-    /**
-     * Subclasses must implement this method to create an Excel HSSFWorkbook
-     * document, given the model.
-     *
-     * @param model
-     *            the model Map
-     * @param workbook
-     *            the Excel workbook to complete
-     * @param request
-     *            in case we need locale etc. Shouldn't look at attributes.
-     * @param response
-     *            in case we need to set cookies. Shouldn't write to it.
-     */
-    protected abstract void buildExcelDocument(Map<String, Object> model, Workbook workbook,
-            HttpServletRequest request, HttpServletResponse response) throws Exception;
+    protected abstract void buildExcelDocument(
+            Map<String, Object> model, XSSFWorkbook workbook, HttpServletRequest request, HttpServletResponse response)
+            throws Exception;
+
 
 }
