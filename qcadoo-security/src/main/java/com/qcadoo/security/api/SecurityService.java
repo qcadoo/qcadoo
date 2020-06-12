01ea264fbcd4a895fe90637cf624e5e5a6326cff
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2020-06-12 14:07:34 +0200

    ANEKS-513 Some fixes.

--- qcadoo-security/src/main/java/com/qcadoo/security/api/SecurityService.java
+++ qcadoo-security/src/main/java/com/qcadoo/security/api/SecurityService.java
@@ -40,23 +40,37 @@ public interface SecurityService {
     String getCurrentUserName();
 
     /**
+     * Returns the name of the currently logged in user or qcadoo_bot
+     *
+     * @return currently logged in user's name
+     */
+    String getCurrentUserOrQcadooBotName();
+
+    /**
      * Returns the id of the currently logged in user
      * 
      * @return currently logged in user's id
      */
     Long getCurrentUserId();
-    
+
+    /**
+     * Returns the id of the currently logged in user or qcadoo_bot
+     *
+     * @return currently logged in user's id
+     */
+    Long getCurrentUserOrQcadooBotId();
+
     /**
      * Checks if given user can access resource with specified access role identifier
      * 
      * @param userEntity
      *            user to check
-     * @param targetRoleIdetifier
+     * @param targetRoleIdentifier
      *            resource access role identifier
      * @return true if given user can access to defined role, false otherwise
      */
-    boolean hasRole(final Entity userEntity, final String targetRoleIdetifier);
+    boolean hasRole(final Entity userEntity, final String targetRoleIdentifier);
 
-    boolean hasCurrentUserRole(final String targetRoleIdetifier);
+    boolean hasCurrentUserRole(final String targetRoleIdentifier);
 
 }
