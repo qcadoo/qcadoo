bcb76df082bb3610e2241d6d7a8673a5d9e56418
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2022-05-24 10:57:56 +0200

    QCADOOCLS-8768 Inform user about any changes regarding his or her account.

--- qcadoo-security/src/main/java/com/qcadoo/security/internal/hooks/UserModelHooks.java
+++ qcadoo-security/src/main/java/com/qcadoo/security/internal/hooks/UserModelHooks.java
@@ -23,29 +23,55 @@
  */
 package com.qcadoo.security.internal.hooks;
 
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
+import com.qcadoo.localization.api.TranslationService;
+import com.qcadoo.mail.api.MailService;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.security.api.SecurityService;
 import com.qcadoo.security.constants.UserFields;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+import java.util.Objects;
 
 @Service
 public class UserModelHooks {
 
-    private static final String SELF_DELETION_ERROR = "security.message.error.selfDeletion";
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserModelHooks.class);
+
+    private static final String L_SELF_DELETION_ERROR = "security.message.error.selfDeletion";
+
+    private static final String L_QCADOOSECURITY_USER = "qcadooSecurity.user.";
+
+    private static final String L_LABEL = ".label";
+
+    private static final String L_QCADOOSECURITY_USER_IMPORTANT_FIELD_CHANGE_MAIL_TOPIC = "qcadooSecurity.user.importantFieldChange.mailTopic";
+
+    private static final String L_QCADOOSECURITY_USER_IMPORTANT_FIELD_CHANGE_MAIL_BODY = "qcadooSecurity.user.importantFieldChange.mailBody";
+
 
     @Autowired
     private SecurityService securityService;
 
+    @Autowired
+    private MailService mailService;
+
+    @Autowired
+    private TranslationService translationService;
+
     public boolean preventSelfDeletion(final DataDefinition userDD, final Entity user) {
         if (ObjectUtils.equals(securityService.getCurrentUserId(), user.getId())) {
-            user.addGlobalError(SELF_DELETION_ERROR);
+            user.addGlobalError(L_SELF_DELETION_ERROR);
+
             return false;
         }
+
         return true;
     }
 
@@ -56,9 +82,69 @@ public class UserModelHooks {
 
     private void replaceByUserNameIfBlank(final Entity user, final String fieldName) {
         String fieldValue = user.getStringField(fieldName);
+
         if (StringUtils.isBlank(fieldValue)) {
             user.setField(fieldName, user.getStringField(UserFields.USER_NAME));
         }
+    }
+
+    public void checkIfEmailNotificationShouldBeSent(final DataDefinition userDD, final Entity user) {
+        Long userId = user.getId();
+
+        if (Objects.nonNull(userId)) {
+            Entity userFromDB = userDD.get(userId);
+
+            checkIfEmailNotificationShouldBeSent(userFromDB, user, UserFields.PASSWORD);
+            checkIfEmailNotificationShouldBeSent(userFromDB, user, UserFields.EMAIL);
+        }
+    }
+
+    private void checkIfEmailNotificationShouldBeSent(final Entity oldUser, final Entity newUser, final String fieldName) {
+        boolean passwordMode = UserFields.PASSWORD.equals(fieldName);
+
+        String oldString = oldUser.getStringField(fieldName);
+
+        if (StringUtils.isEmpty(oldString)) {
+            oldString = "";
+        }
+
+        String newString = newUser.getStringField(fieldName);
+
+        if (StringUtils.isEmpty(newString)) {
+            if (passwordMode) {
+                //there was no field change if new string is null and this is password field
+                newString = oldString;
+            } else {
+                newString = "";
+            }
+        }
+
+        if (!oldString.equals(newString)) {
+            String email = oldUser.getStringField(UserFields.EMAIL);
+
+            if (StringUtils.isNotEmpty(email)) {
+                sendEmail(email, fieldName);
+            } else {
+                //only log info
+                LOGGER.error("Important user field change has been made, but user '{}' has not defined valid email!", new Object[]{oldUser.getStringField(UserFields.USER_NAME)});
+                //do not log password value for password changes
+
+                if (!passwordMode) {
+                    LOGGER.error("The change was made for field '{}', old value is '{}' and new value is '{}'.", new Object[]{fieldName, oldString, newString});
+                }
+            }
+        }
+    }
+
+    private void sendEmail(final String email, final String fieldName) {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        String fieldTranslated = translationService.translate(L_QCADOOSECURITY_USER + fieldName + L_LABEL, locale);
+
+        String topic = translationService.translate(L_QCADOOSECURITY_USER_IMPORTANT_FIELD_CHANGE_MAIL_TOPIC, locale, fieldTranslated);
+        String body = translationService.translate(L_QCADOOSECURITY_USER_IMPORTANT_FIELD_CHANGE_MAIL_BODY, locale, fieldTranslated);
+
+        mailService.sendEmail(email, topic, body);
     }
 
 }
