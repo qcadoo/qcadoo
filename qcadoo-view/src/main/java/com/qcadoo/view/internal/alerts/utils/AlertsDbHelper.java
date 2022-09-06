7c7e25dcd4c32927edad257fc4245e7f49d9cdd5
Łukasz Poniewierski <lukasz.poniewierski@qcadoo.com>
2022-09-06 16:16:45 +0200

    ANEKS-577

--- qcadoo-view/src/main/java/com/qcadoo/view/internal/alerts/utils/AlertsDbHelper.java
+++ qcadoo-view/src/main/java/com/qcadoo/view/internal/alerts/utils/AlertsDbHelper.java
@@ -1,12 +1,5 @@
 package com.qcadoo.view.internal.alerts.utils;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.google.common.collect.Lists;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
@@ -17,6 +10,13 @@ import com.qcadoo.view.constants.AlertFields;
 import com.qcadoo.view.constants.QcadooViewConstants;
 import com.qcadoo.view.constants.ViewedAlertFields;
 import com.qcadoo.view.internal.alerts.model.AlertDto;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class AlertsDbHelper {
@@ -27,65 +27,81 @@ public class AlertsDbHelper {
     @Autowired
     private UserService userService;
 
-    public void createViewedAlerts(final List<AlertDto> alerts) {
+    public void createViewedAlerts(final List<AlertDto> alertDtos) {
         Entity user = userService.getCurrentUserEntity();
-        alerts.forEach(a -> createViewedAlert(a.getId(), user));
+        
+        alertDtos.forEach(alertDto -> createViewedAlert(alertDto.getId(), user));
     }
 
-    private void createViewedAlert(final Long id, final Entity user) {
-        Entity viewedAlert = getViewdAlertDD().create();
-        Entity alert = dataDefinitionService.get(QcadooViewConstants.PLUGIN_IDENTIFIER, QcadooViewConstants.MODEL_ALERT).get(id);
+    private void createViewedAlert(final Long alertId, final Entity user) {
+        Entity alert = getAlertDD().get(alertId);
+
+        Entity viewedAlert = getViewedAlertDD().create();
+
         viewedAlert.setField(ViewedAlertFields.USER, user);
         viewedAlert.setField(ViewedAlertFields.ALERT, alert);
+
         viewedAlert.getDataDefinition().save(viewedAlert);
+    }
+
+    public List<AlertDto> getAlerts() {
+        List<AlertDto> alertDtos = Lists.newArrayList();
+
+        getAlertsForUser().forEach(alert -> mapToDTO(alert, alertDtos));
+
+        return alertDtos;
     }
 
     private List<Entity> getAlertsForUser() {
         Entity user = userService.getCurrentUserEntity();
 
         List<Entity> result = Lists.newArrayList();
+
         DateTime currentDate = DateTime.now();
-        if (user != null) {
+
+        if (Objects.nonNull(user)) {
             List<Entity> alerts = getAlertDD().find()
                     .add(SearchRestrictions.ge(AlertFields.EXPIRATION_DATE, currentDate.toDate())).list().getEntities();
-            List<Entity> viewedAlerts = getViewdAlertDD().find()
-                    .add(SearchRestrictions.belongsTo(ViewedAlertFields.USER, user)).list().getEntities();
+            List<Entity> viewedAlerts = getViewedAlertDD().find()
+                    .add(SearchRestrictions.belongsTo(ViewedAlertFields.USER, user))
+                    .list().getEntities();
+
             result.addAll(alerts.stream().filter(alert -> viewedAlerts.stream()
-                    .noneMatch(va -> va.getBelongsToField(ViewedAlertFields.ALERT).getId() == alert.getId()))
+                            .noneMatch(viewedAlert -> viewedAlert.getBelongsToField(ViewedAlertFields.ALERT).getId().equals(alert.getId())))
                     .collect(Collectors.toList()));
         }
+
         return result;
     }
 
-    public List<AlertDto> getAlerts() {
-        List<AlertDto> alerts = Lists.newArrayList();
-        getAlertsForUser().forEach(a -> mapToDTO(a, alerts));
-        return alerts;
+    private void mapToDTO(final Entity alert, List<AlertDto> alertDtos) {
+        AlertDto alertDto = new AlertDto();
+
+        alertDto.setId(alert.getId());
+        alertDto.setType(alert.getStringField(AlertFields.TYPE));
+        alertDto.setMessage(alert.getStringField(AlertFields.MESSAGE));
+        alertDto.setSound(alert.getBooleanField(AlertFields.SOUND));
+
+        alertDtos.add(alertDto);
     }
 
-    private void mapToDTO(Entity a, List<AlertDto> alerts) {
-        AlertDto alert = new AlertDto();
-        alert.setType(a.getStringField(AlertFields.TYPE));
-        alert.setMessage(a.getStringField(AlertFields.MESSAGE));
-        alert.setSound(a.getBooleanField(AlertFields.SOUND));
-        alert.setId(a.getId());
-        alerts.add(alert);
-    }
+    void registerAlert(final AlertDto alertDto) {
+        Entity alert = getAlertDD().create();
 
-    void registerAlert(final AlertDto alert) {
-        Entity alertEntity = getAlertDD().create();
-        alertEntity.setField(AlertFields.TYPE, alert.getType());
-        alertEntity.setField(AlertFields.MESSAGE, alert.getMessage());
-        alertEntity.setField(AlertFields.EXPIRATION_DATE, alert.getExpirationDate());
-        alertEntity.setField(AlertFields.SOUND,alert.isSound());
-        alertEntity.getDataDefinition().save(alertEntity);
+        alert.setField(AlertFields.TYPE, alertDto.getType());
+        alert.setField(AlertFields.MESSAGE, alertDto.getMessage());
+        alert.setField(AlertFields.EXPIRATION_DATE, alertDto.getExpirationDate());
+        alert.setField(AlertFields.SOUND,alertDto.isSound());
+
+        alert.getDataDefinition().save(alert);
     }
 
     private DataDefinition getAlertDD() {
         return dataDefinitionService.get(QcadooViewConstants.PLUGIN_IDENTIFIER, QcadooViewConstants.MODEL_ALERT);
     }
 
-    private DataDefinition getViewdAlertDD() {
+    private DataDefinition getViewedAlertDD() {
         return dataDefinitionService.get(QcadooViewConstants.PLUGIN_IDENTIFIER, QcadooViewConstants.MODEL_VIEWED_ALERT);
     }
+
 }
