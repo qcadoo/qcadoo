04e19d6e8efbf103e6b9c39cf75106b67c1d0c7f
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2024-05-06 14:19:48 +0200

    QCADOOMES-2606

--- qcadoo-view/src/main/resources/qcadooView/public/js/core/qcd/utils/multiUpload.js
+++ qcadoo-view/src/main/resources/qcadooView/public/js/core/qcd/utils/multiUpload.js
@@ -1,3 +1,3 @@
 var QCDMultiUpload = {};
 
-QCDMultiUpload.acceptFileTypes = /(\.|\/)(ezpx|stp|btw|dxf|zip|gif|jpe?g|png|pdf|xls|xlsx|dwg|ipt|iam|idw|docx?|txt|csv|xml|odt|ods|tiff?)$/i;
+QCDMultiUpload.acceptFileTypes = /(\.|\/)(plt|ezpx|stp|btw|dxf|zip|gif|jpe?g|png|pdf|xls|xlsx|dwg|ipt|iam|idw|docx?|txt|csv|xml|odt|ods|tiff?)$/i;
