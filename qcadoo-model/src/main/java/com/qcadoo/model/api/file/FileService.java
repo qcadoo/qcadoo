2dea80780a3a9fce57765d4a3d7897709c9995fb
Krzysztof Nadolski <krzysztof.nadolski@qcadoo.com>
2024-09-02 18:20:39 +0200

    QCADOOCLS-10530

--- qcadoo-model/src/main/java/com/qcadoo/model/api/file/FileService.java
+++ qcadoo-model/src/main/java/com/qcadoo/model/api/file/FileService.java
@@ -33,41 +33,38 @@ import java.util.List;
 
 /**
  * Service for managing files.
- * 
+ *
  * @since 0.4.1
  */
 public interface FileService {
 
     /**
      * Returns name of the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      * @return name
      */
     String getName(final String path);
 
     /**
      * Returns last modification date of the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      * @return last modification date
      */
     String getLastModificationDate(final String path);
 
     /**
      * Returns URL for the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      * @return URL
      */
     String getUrl(final String path);
 
     /**
      * Returns path for the file from given URL.
-     * 
+     *
      * @param url
      * @return path
      */
@@ -75,45 +72,40 @@ public interface FileService {
 
     /**
      * Returns stream of the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      * @return stream
      */
     InputStream getInputStream(final String path);
 
     /**
      * Create file from given uploaded file.
-     * 
-     * @param multipartFile
-     *            uploaded file
+     *
+     * @param multipartFile uploaded file
      * @return path
      */
     String upload(final MultipartFile multipartFile) throws IOException;
 
     /**
      * Returns content type of the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      * @return content type
      */
     String getContentType(final String path);
 
     /**
      * Create empty export file with given name.
-     * 
-     * @param filename
-     *            filename
+     *
+     * @param filename filename
      * @return File
      */
     File createExportFile(String filename);
 
     /**
      * Create empty report file with given name.
-     * 
-     * @param filename
-     *            filename
+     *
+     * @param filename filename
      * @return File
      * @throws IOException
      */
@@ -121,20 +113,17 @@ public interface FileService {
 
     /**
      * Remove the file from given path.
-     * 
-     * @param path
-     *            path
+     *
+     * @param path path
      */
     void remove(String path);
 
     /**
      * Update report file name for given report entity
-     * 
+     *
      * @param entity
-     * @param dateFieldName
-     *            report date field name
-     * @param name
-     *            translation code for language specific file name
+     * @param dateFieldName report date field name
+     * @param name          translation code for language specific file name
      * @return updated entity
      */
     Entity updateReportFileName(Entity entity, String dateFieldName, String name);
@@ -143,12 +132,9 @@ public interface FileService {
      * Update report file name for given report entity
      *
      * @param entity
-     * @param dateFieldName
-     *            report date field name
-     * @param name
-     *            translation code for language specific file name
-     * @param args
-     *            translation args
+     * @param dateFieldName report date field name
+     * @param name          translation code for language specific file name
+     * @param args          translation args
      * @return updated entity
      */
     Entity updateReportFileName(Entity entity, String dateFieldName, String name, String... args);
@@ -156,14 +142,14 @@ public interface FileService {
     /**
      * Compress documents to newly created zip file.
      *
-     * @param documents
-     *            documents to be compress
-     * @param removeCompressed
-     *            if true removes documents after compression
+     * @param documents        documents to be compress
+     * @param removeCompressed if true removes documents after compression
      * @return created zip file
      * @throws IOException
      */
     File compressToZipFile(List<File> documents, boolean removeCompressed) throws IOException;
 
     File getFileFromFilenameWithRandomDirectory(final String filename);
+
+    File getFileFromFilenameWithRandomDirectory(final String filename, final boolean withDate);
 }
